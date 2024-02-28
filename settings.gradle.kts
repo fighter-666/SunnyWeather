54d6769d649cbe547364aabc56784d94826cafd7
wudaoman <977657115@qq.com>
2024-02-28 11:11:55 +0800

    初始化布局

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,50 @@
 pluginManagement {
     repositories {
+        maven {
+            url =uri("http://maven.aliyun.com/nexus/content/groups/public/")
+            isAllowInsecureProtocol = true}
+        maven {
+            url =uri("http://maven.aliyun.com/nexus/content/repositories/jcenter")
+            isAllowInsecureProtocol = true}
+        maven {
+            url =uri("http://maven.aliyun.com/nexus/content/repositories/google")
+            isAllowInsecureProtocol = true}
+        maven {
+            url =uri("http://maven.aliyun.com/nexus/content/repositories/gradle-plugin")
+            isAllowInsecureProtocol = true}
+        maven {
+            url =uri("https://jitpack.io")
+            isAllowInsecureProtocol = true}
+
+        gradlePluginPortal()
         google()
         mavenCentral()
-        gradlePluginPortal()
 
     }
 }
+
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven {
+            url =uri("http://maven.aliyun.com/nexus/content/groups/public/")
+            isAllowInsecureProtocol = true}
+        maven {
+            url =uri("http://maven.aliyun.com/nexus/content/repositories/jcenter")
+            isAllowInsecureProtocol = true}
+        maven {
+            url =uri("http://maven.aliyun.com/nexus/content/repositories/google")
+            isAllowInsecureProtocol = true}
+        maven {
+            url =uri("http://maven.aliyun.com/nexus/content/repositories/gradle-plugin")
+            isAllowInsecureProtocol = true}
+        maven {
+            url =uri("https://jitpack.io")
+            isAllowInsecureProtocol = true}
+
     }
 }
 
